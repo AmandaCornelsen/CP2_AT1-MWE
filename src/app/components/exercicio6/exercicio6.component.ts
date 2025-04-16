d7003b16c360aa703389b8ce1dc55ef52094f42b
AmandaCornelsen <cornelsen089@gmail.com>
2025-04-15 22:31:10 -0300

    Exercício 6 - Lista de objetos com for

--- src/app/components/exercicio6/exercicio6.component.ts
+++ src/app/components/exercicio6/exercicio6.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-exercicio6',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './exercicio6.component.html',
   styleUrl: './exercicio6.component.css'
 })
 export class Exercicio6Component {
-
+  usuarios = [{nome: "Ana", idade: 25 }, {nome: "Carlos", idade: 30}];
 }
