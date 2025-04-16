d9e561f62d01edb94f587d3573512ecf9bd42f6f
AmandaCornelsen <cornelsen089@gmail.com>
2025-04-15 21:57:12 -0300

    exercício 1 - usando o switch

--- src/app/components/exercicio1/exercicio1.component.ts
+++ src/app/components/exercicio1/exercicio1.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-exercicio1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './exercicio1.component.html',
   styleUrl: './exercicio1.component.css'
 })
 export class Exercicio1Component {
 
+  mes: number = 1;
+
+  trocarValor(event: Event): void {
+    const elemento = event.target as HTMLInputElement;
+    this.mes = parseInt(elemento.value, 10);
+  }
+
 }
+
