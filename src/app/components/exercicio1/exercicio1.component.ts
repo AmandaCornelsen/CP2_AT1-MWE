1d7742bc625febd0a40fce2a6789d844622042ea
AmandaCornelsen <cornelsen089@gmail.com>
2025-04-15 21:56:18 -0300

    exercício 1 - uso do switch

---
+++ src/app/components/exercicio1/exercicio1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio1',
+  standalone: true,
+  imports: [],
+  templateUrl: './exercicio1.component.html',
+  styleUrl: './exercicio1.component.css'
+})
+export class Exercicio1Component {
+
+}
